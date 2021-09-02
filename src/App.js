5edcaf1ceb52a8c8772f85ac4155676d7f59002c
Benjamin Austin <bnjmn80@gmail.com>
2021-09-02 16:13:55 -0600

    Utilization of React.memo() and useCallback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
+import Button from './components/UI/Button/Button.js';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("%c App component-function runned!", "color: red;");
+
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => {
+      return !prevShowParagraph;
+    });
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
